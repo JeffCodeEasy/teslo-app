cd6eb6387c81bed0c393866498a636af542c5275
Jeremy <jeffcode.easy@gmail.com>
2025-05-29 17:20:06 -0500

    campos de formulario

--- lib/features/shared/infraestructure/inputs/inputs.dart
+++ lib/features/shared/infraestructure/inputs/inputs.dart
@@ -2,3 +2,7 @@ export 'email.dart';
 export 'password.dart';
 export 'fullname.dart';
 export 'reptpassword.dart';
+export 'slug.dart';
+export 'title.dart';
+export 'price.dart';
+export 'stock.dart';
